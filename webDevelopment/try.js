09c8d8de971be6f15e896647f9fe62a9f9b64b05
RazanRahhal <razan.rahhal@gmail.com>
2023-02-03 11:07:28 -0500

    Add webDevelopment folder

---
+++ webDevelopment/try.js
@@ -0,0 +1,12 @@
+//this is a comment
+alert("welcome to my website");
+var user=prompt("enter your name");
+var result=confirm("are you ready?")
+ function color(){
+   var dd1= document.getElementById('id1');
+   var dd2= document.getElementById('id2');
+   var dd3= document.getElementById('id3');
+ dd1.className="changeColor";
+ dd2.className="changeColor";
+ dd3.className="changeColor";
+ }
